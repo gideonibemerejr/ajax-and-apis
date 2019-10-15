cba72860b844dbc9ec2c8f8626b453de8f5cafdc
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-15 15:13:01 -0500

    Update App.js to include route to Home Page component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-// TODO: import {HomePage, }
+// TODO: import IceBox, DesignKnowledge, CoinPrice, Uova, UnsplashSearch, Figure, Instafeed, PitchforkScraper, MinimalTwitter
 
+import { HomePage } from './Pages'
 
 function App() {
   return (
     <Router>
       <Switch>
-        
+        {/* Route to Home Page */}
+        <Route exact path='/' component={HomePage} />
+        {/* Route to Ice Box */}
+        {/* Route to Design Knowledge */}
+        {/* Route to Coin Price */}
+        {/* Route to Uova */}
+        {/* Route to Unsplash Search */}
+        {/* Route to Figure */}
+        {/* Route to Instafeed */}
+        {/* Route to Pitchfork Scraper*/}
+        {/* Route to Minimal Twitter */}
       </Switch>
     </Router>
   )
