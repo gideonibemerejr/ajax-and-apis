c6cc4a979f0123eae281ac091aaaed123fde6637
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-17 00:52:51 -0500

    Update Header compolete

--- src/Pages/Homepage/components/Hero/index.jsx
+++ src/Pages/Homepage/components/Hero/index.jsx
@@ -4,12 +4,13 @@ import styles from './Hero.module.css'
 const Hero = () => {
   return (
     <section className={styles.section}>
+      <div className={styles.hero_img} />
       <article className={styles.article}>
         <h1 className={styles.h1}>Ajax + APIs</h1>
         <p className={styles.p}>
           With a short course by{' '}
           <a
-            className='HomePage-link'
+            className={styles.link}
             target='_blank'
             rel='noopener noreferrer'
             href='https://www.superhi.com'
@@ -19,7 +20,7 @@ const Hero = () => {
           , I learned how to work with Ajax and APIs in a creative, fun way.
           After each lesson, I refactored and added to each application with{' '}
           <a
-            className='HomePage-link'
+            className={styles.link}
             target='_blank'
             rel='noopener noreferrer'
             href='https://www.reactjs.org'
@@ -28,6 +29,16 @@ const Hero = () => {
           </a>
           .
         </p>
+        <blockquote cite='https://www.superhi.com/cources/ajax-and-apis'>
+          <p>
+            Whenever someone says “Twitter has an API”, what they mean is that
+            Twitter has a particular way to let other sites remix their data to
+            create brand new projects.
+          </p>
+          <footer>
+            -SuperHi, <cite>Ajax + APIs</cite>
+          </footer>
+        </blockquote>
       </article>
     </section>
   )
